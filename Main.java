1a5dff9e0446ad071413e59f0afee4fb510f8156
blgsn <111683241+blgsn@users.noreply.github.com>
2022-11-20 23:12:40 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+	 public static void main(String[] args) {
+		 PatikaStore patikaStore = new PatikaStore("PatikaStore");
+	        new BrandSpecifications(patikaStore);
+	        new UserManagement(patikaStore);
+	    }
+
+}
