a6ed08751b2907bb75a2a1df019b41f57b68816c
blgsn <111683241+blgsn@users.noreply.github.com>
2022-11-20 17:02:36 +0300

    Delete Notebook.java

--- Notebook.java
+++
@@ -1,9 +0,0 @@
-package product.readyType;
-import product.baseType.Hardware;
-import product.ProductType;
-
-public class Notebook extends Hardware {
-    public Notebook(String productName, int price) {
-        super(ProductType.Notebook, productName, price);
-    }
-}
