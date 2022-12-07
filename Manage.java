f02f5ad87da43629ca0d6576f4f8ef8e77b1b33d
blgsn <111683241+blgsn@users.noreply.github.com>
2022-12-07 14:06:37 +0300

    Update Manage.java

--- Manage.java
+++ Manage.java
@@ -4,14 +4,14 @@ import product.ProductInputs;
 
 import java.util.List;
 import java.util.TreeSet;
-
+//method of list in alphabetical order using treeset
 public class Manage {
 	private TreeSet<Brand> brands;
     public Manage(){
         brands = new TreeSet<>();
        
     }
-
+//add product according to id and name
     public Brand addBrand(String brandName){
         int id = brands.size();
         
@@ -19,7 +19,7 @@ public class Manage {
         brands.add(resultBrand);
         return resultBrand;
     }
-
+//checks if it is equal to the brands in the list
     public Brand getBrand(String brandName){
         for (Brand brand : brands){
             if (brand.getBrandName().equals(brandName)){
@@ -32,14 +32,11 @@ public class Manage {
         return resultBrand;
     }
     
-  
-        
-     
-        
     public TreeSet<Brand> getAllBrands(){
         return this.brands;
     }
 
+//adds the brand to be added in alphabetical order
     public TreeSet<ProductInputs> getAllProducts(){
         TreeSet<ProductInputs> products = new TreeSet<>();
         for (Brand brand : brands){
